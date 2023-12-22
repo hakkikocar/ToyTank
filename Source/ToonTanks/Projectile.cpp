da1b8fdd38b4d1c4db15fc2a85aa464cfa037dc9
Hakkı Koçar <18701025@mersin.edu.tr>
2023-12-22 16:41:43 +0300

    health and game mode
    maked health component
    added apply damage
    calculated health
    destroyed projectile
    maked new game mode
    set default pawn

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Projectile.h"
+#include "GameFramework/DamageType.h"
+#include "Kismet/GameplayStatics.h"
+
 
 // Sets default values
 AProjectile::AProjectile()
@@ -35,12 +38,17 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,FVector NormalImpulse, const FHitResult& Hit)
 {
-	UE_LOG(LogTemp, Warning, TEXT("OnHit"));
-	UE_LOG(LogTemp, Warning, TEXT("HitComp: %s"), *HitComp->GetName());
-	UE_LOG(LogTemp, Warning, TEXT("OtherActor: %s"), *OtherActor->GetName());
-	UE_LOG(LogTemp, Warning, TEXT("OtherComp: %s"), *OtherComp->GetName());
+	auto MyOwner = GetOwner();
+	if (MyOwner==nullptr) return;
 
-	
+	auto MyOwnerInstigator=	MyOwner->GetInstigatorController();
+	auto DamageTypeCalass = UDamageType::StaticClass();
+
+	if (OtherActor && OtherActor!= this && OtherActor != MyOwner)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor,Damage,MyOwnerInstigator,MyOwner,DamageTypeCalass);
+		Destroy();
+	}
 }
 
 
